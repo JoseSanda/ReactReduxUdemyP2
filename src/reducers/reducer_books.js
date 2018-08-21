7a289033de1f970f766b8a0b367c502e2f548417
Tono <jfsanda@gmail.com>
2018-08-22 00:09:52 +0200

    Incluido el BooksReducer.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function BooksReducer() {
+  return (
+    [{title: 'Javascript: The Good Parts'},
+    {title: 'Eloquent Ruby'},
+    {title: 'Harry Potter'},
+    {title: 'Origen'}]
+  );
+}
